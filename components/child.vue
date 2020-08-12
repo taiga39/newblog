9b8472f070496ba7f949280aa78c55859f527cc6
taiga39 <ishikawa.tigers@gmail.com>
2020-08-12 22:55:21 +0900

    トップページ修正

---
+++ components/child.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+    <div class="post-preview" v-for="n of jsonlength" :key="n">
+        <router-link v-bind:to="'/' + catename + '/' + jsontab[n-1].link">
+            <h2 class="p">
+                {{jsontab[n-1].title}}
+            </h2>
+        </router-link>
+        <p class="post-meta" style="padding-left:1em">
+            Posted 
+            on {{jsontab[n-1].date}}  
+        </p>
+    </div>
+    <div class="clearfix">
+        <router-link class="btn btn-primary float-right" v-bind:to="'/' + catename">More Posts &rarr;</router-link>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.graph-h{
+    text-align:center;
+    padding:1rem;
+    font-size:3em;
+    font-weight:bold;
+}
+</style>
+
+<script>
+export default {
+    data(){
+        return{
+            jsonlength: 0,
+            jsontab:""
+        }
+    },
+    props:["jsondata","catename"],
+    mounted() {
+        if(this.jsondata != null){
+            if(JSON.parse(JSON.stringify(Object.keys(this.jsondata).length)) < 5){
+                this.jsonlength = JSON.parse(JSON.stringify(Object.keys(this.jsondata).length))
+            }else{
+                this.jsonlength = 5
+            }
+            this.jsontab = Object.values(this.jsondata)
+        }
+    },
+    watch: {
+        jsondata:function(one){
+            if(JSON.parse(JSON.stringify(Object.keys(one).length)) < 5){
+                this.jsonlength = JSON.parse(JSON.stringify(Object.keys(one).length))
+            }else{
+                this.jsonlength = 5
+            }
+            this.jsontab = Object.values(one);
+        }
+    }
+}
+</script>
+
+<style scoped>
+@media screen and (max-width:550px) {
+  h2 {
+    font-size: 1.3em;
+  }
+}
+
+.post-preview{
+    width:80%;
+    margin:30px auto;
+    cursor: pointer;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,64 +15,31 @@
   </header>
   <div class="container">
     <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Man must explore, and this is exploration at its greatest
-            </h2>
-            <h3 class="post-subtitle">
-              Problems look mighty small from 150 miles up
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
+      <div class="col-lg-8 col-md-10 mx-auto" id="examlpe-1">
+        <div class="title-box1-title"><span class="title-box1-title-span">30歳</span>まで残り</div>
+        <div class="title-box1">
+          <span class="nokori">{{termday}}</span><span class="days">days</span>
         </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-            </h2>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
+        <div style="display:flex;">
+          <input id="all" type="radio" name="tab_item" checked>
+          <label class="tab_item" for="all" v-on:click="change(1)" v-bind:class="{'active': isActive === 1}">レポート</label>
+          <input id="programming" type="radio" name="tab_item">
+          <label class="tab_item" for="programming" v-on:click="change(2)" v-bind:class="{'active': isActive === 2}">ブログ</label>
+          <input id="design" type="radio" name="tab_item">
+          <label class="tab_item" for="design" v-on:click="change(3)" v-bind:class="{'active': isActive === 3}">テック</label>
         </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Science has not yet mastered prophecy
-            </h2>
-            <h3 class="post-subtitle">
-              We predict too much for the next year and yet far too little for the next ten.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Failure is not an option
-            </h2>
-            <h3 class="post-subtitle">
-              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
-        </div>
-        <hr>
-        <!-- Pager -->
-        <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
+        <ul style="padding:0">
+          <li v-if="isActive === 1" style="list-style:none;">
+            <Child :jsondata="$store.state.report" :catename="report" /> 
+          </li>
+          <li v-else-if="isActive === 2" style="list-style:none;">
+            <Child :jsondata="$store.state.blog" :catename="blog" /> 
+          </li>
+          <li v-else-if="isActive === 3" style="list-style:none;">
+            <Child :jsondata="$store.state.tech" :catename="tech"/> 
+          </li>
+        </ul>
+
       </div>
     </div>
   </div>
@@ -81,11 +48,45 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import Child from "@/components/child.vue";
 import '@/assets/css/clean-blog.min.css'
 import "@/assets/css/bootstrap.min.css";
 import "@/assets/css/all.min.css";
 
-export default Vue.extend({})
+export default {
+  data(){
+    return{
+      isActive: 1,
+      report: "report",
+      blog:"blog",
+      tech:"tech",
+      termday:10000
+    }
+  },
+  head: {
+    meta: [
+      { name: 'description', content: 'Gal☆Blog（ギャルブログ）のトップページです' },
+      { property: 'og:title', content: 'Gal☆Blog（ギャルブログ）' },
+      { property: 'og:description', content: 'Gal☆Blog（ギャルブログ）のトップページです' },
+      // { property: 'og:image', content: 'Gal☆Blog（ギャルブログ）' },
+
+    ],
+  },
+  methods: {
+    change: function(num: number){
+      this.isActive = num
+    }
+  },
+  mounted(){
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var date1: any = new Date(year ,month ,day);
+    var date2: any = new Date(2023, 5, 12);
+    this.termday = (date2 - date1) / 86400000;
+  }
+}
 </script>
 
 <style scoped>
@@ -93,5 +94,74 @@ header{
   background-image:url('~@/assets/images/back0.jpg');
   background-repeat:none;
   background-size:cover;
+}
+.title-box1-title{
+  width:70%;
+  font-size:1.5vw;
+  margin:0 auto;
+}
+.title-box1-title-span{
+  font-size:3vw;
+  font-weight:500;
+}
+.title-box1{
+  border: 1px solid #212529;
+  background:#212529;
+  color: white;
+  padding: 10px 10px;
+  text-align: center;
+  margin:0 auto 50px;
+  width:70%;
+}
+.nokori{
+  color:red;font-size:6vw;font-weight:800;
+}
+.days{
+  font-size:2vw;
+}
+.tab_item {
+  width: calc(100%/3);
+  height: 50px;
+  border-bottom: 3px solid #5ab4bd;
+  background-color: white;
+  line-height: 50px;
+  font-size: 16px;
+  text-align: center;
+  color: #565656;
+  display: block;
+  text-align: center;
+  font-weight: bold;
+  transition: all 0.2s ease;
+  cursor: pointer;
+}
+.tab_item:hover {
+  opacity: 0.75;
+}
+input[name="tab_item"] {
+  display: none;
+}
+
+.active{
+  background-color: #5ab4bd;
+  color: #fff;
+}
+@media screen and (max-width:550px) {
+  .tab_item {
+    height:10vw;
+    font-size: 0.8em;
+    line-height: 10vw;
+  }
+  .title-box1-title{
+    font-size:4vw;
+  }
+  .title-box1-title-span{
+    font-size:6vw;
+  }
+  .nokori{
+    font-size:10vw;
+  }
+  .days{
+    font-size:5vw;
+  }
 }
 </style>
