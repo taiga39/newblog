da09eac048f81e385cca724bb3023be52bab07c2
taiga39 <ishikawa.tigers@gmail.com>
2020-08-26 22:20:34 +0900

    修正

--- pages/tech/_id.vue
+++ pages/tech/_id.vue
@@ -7,7 +7,6 @@
             <div class="col-lg-8 col-md-10 mx-auto">
             <div class="post-heading">
                 <h1>{{blogs[0].fields.title}}</h1>
-                <h2 class="subheading"></h2>
                 <span class="meta">Posted on {{(blogs[0].fields.publishDate).slice(0,-12)}}</span>
             </div>
             </div>
